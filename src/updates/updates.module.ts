7e773bd380e6c651d29cc3fa46976e14f43216c0
holfizz <gorlach7v@gmail.com>
2026-04-18 12:12:15 +0300

    upd

--- src/updates/updates.module.ts
+++ src/updates/updates.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
+import { PrismaModule } from '../prisma/prisma.module'
 import { UpdatesController } from './updates.controller'
 
 @Module({
+	imports: [PrismaModule],
 	controllers: [UpdatesController],
 })
 export class UpdatesModule {}
